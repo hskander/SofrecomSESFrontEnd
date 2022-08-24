37f6d4bae4d5731eba1f1666cd0ead573f09bc99
Skander HAMDI <s.hamdi@sofrecom-tunisie.com>
2022-08-24 16:21:50 +0100

    '12'

--- src/app/all-modules/jobs/jobs-routing.module.ts
+++ src/app/all-modules/jobs/jobs-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { JobsComponent } from './jobs.component';
 import { ManageJobsComponent } from './manage-jobs/manage-jobs.component';
-import { JobDetailsComponent } from './manage-jobs/job-details/job-details.component';
-import { JobApplicantsComponent } from './manage-jobs/job-applicants/job-applicants.component';
 
 const routes: Routes = [
   {
@@ -13,14 +11,6 @@ const routes: Routes = [
       {
         path:"manage-jobs",
         component:ManageJobsComponent
-      },
-      {
-        path:"job-details",
-        component:JobDetailsComponent
-      },
-      {
-        path:"job-applicants",
-        component:JobApplicantsComponent
       },
      
     ]
--- src/app/all-modules/jobs/jobs.module.ts
+++ src/app/all-modules/jobs/jobs.module.ts
@@ -5,14 +5,12 @@ import { JobsRoutingModule } from './jobs-routing.module';
 import { JobsComponent } from './jobs.component';
 import { ManageJobsComponent } from './manage-jobs/manage-jobs.component';
 import { DataTablesModule } from 'angular-datatables';
-import { JobDetailsComponent } from './manage-jobs/job-details/job-details.component';
-import { JobApplicantsComponent } from './manage-jobs/job-applicants/job-applicants.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BsDatepickerModule } from 'ngx-bootstrap';
 import { SharingModule } from 'src/app/sharing/sharing.module';
 
 @NgModule({
-  declarations: [JobsComponent, ManageJobsComponent, JobDetailsComponent, JobApplicantsComponent],
+  declarations: [JobsComponent, ManageJobsComponent],
   imports: [
     CommonModule,
     JobsRoutingModule,
